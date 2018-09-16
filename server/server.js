5e70d1575ebf77513c943e3849e3cd34ad694667
avinash195 <avinashsngh735@gmail.com>
2018-09-16 15:16:05 +0530

    added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected");
 
+  socket.emit('newMessage', {
+    from: 'server',
+    text: 'making event from server', 
+    createAt: 310
+  });
+
+  socket.on('createMessage', function(message) {
+    console.log('createMessage', message);
+  })
+
   socket.on("disconnect", () => {
     console.log("User was disconnected");
   });
